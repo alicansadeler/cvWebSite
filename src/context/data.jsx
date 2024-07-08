74ae01e8908587421c590ccb8d824abe1500aefa
alicansadeler <alicansadeler@gmail.com>
2024-07-08 20:15:57 +0300

    datalar cekildi/resim projeye eklendi

--- src/context/data.jsx
+++ src/context/data.jsx
@@ -1,11 +1,44 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const myContext = createContext();
 
 export const MyContextProvider = ({ children }) => {
+  const [dataEU, setDataEU] = useState([]);
   const [dataTR, setDataTR] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://668bdc690b61b8d23b0b7836.mockapi.io/acs/activeeu")
+      .then(function (response) {
+        // handle success
+        setDataEU(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://668bdc690b61b8d23b0b7836.mockapi.io/acs/activetr")
+      .then(function (response) {
+        // handle success
+        setDataTR(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
   return (
-    <myContext.Provider value={{ dataTR, setDataTR }}>
+    <myContext.Provider value={{ dataEU, dataTR }}>
       {children}
     </myContext.Provider>
   );
