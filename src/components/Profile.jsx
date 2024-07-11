c144f3bfa093ade987da0bbc1662a43e082de87d
alicansadeler <alicansadeler@gmail.com>
2024-07-11 23:37:46 +0300

    profile css düzenlendi

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,22 +10,27 @@ export const Profile = () => {
   const profileData = data[0][0].profiles;
 
   return (
-    <>
-      <div>
-        <h2 className="text-5xl font-semibold  text-left py-0">Profile</h2>
-        <h2>{profileData.title1}</h2>
-        <ul>
-          {Object.entries(profileData.items[0].details).map(([key, value]) => (
-            <li key={key}>
-              {key} : {value}
-            </li>
-          ))}
-        </ul>
+    <div className="space-y-8">
+      <h2 className="text-5xl font-semibold text-left  py-0">Profile</h2>
+      <div className="flex justify-between">
+        <div className="space-y-8 text-left">
+          <h3>{profileData.title1}</h3>
+          <ul className="flex flex-col w-full max-w-2xl">
+            {Object.entries(profileData.items[0].details).map(
+              ([key, value]) => (
+                <li key={key} className="flex mb-0">
+                  <div className="font-semibold text-[18px] w-1/3 ">{key}</div>
+                  <div className="p-0 flex-1 min-h-[40px]">{value}</div>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+        <div className="space-y-8 w-[580px] text-left">
+          <h3>{profileData.title2}</h3>
+          <p>{profileData.items[1].text}</p>
+        </div>
       </div>
-      <div>
-        <h3>{profileData.title2}</h3>
-        <p>{profileData.items[1].text}</p>
-      </div>
-    </>
+    </div>
   );
 };
