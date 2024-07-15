cfd85175ac80adbf737193ea6184fa78686b98ee
alicansadeler <alicansadeler@gmail.com>
2024-07-15 22:18:47 +0300

    logger çıkarıldı

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware } from "redux";
 import { legacy_createStore } from "redux";
-import logger from "redux-logger";
 import { thunk } from "redux-thunk";
 import { reducer } from "./reducers/reducer";
 
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk);
 
 export const myStore = legacy_createStore(reducer, middleware);
